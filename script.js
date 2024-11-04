013830a71297d2f725dac1908b2fa008d8e615b7
tauheed0110 <tauheed0110@gmail.com>
2024-11-04 15:13:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+    const n = str.length;
+
+    for (let i = 0; i < n; i++) {
+        let flag = false;
+        for (let j = 0; j < n; j++) {
+            if (i === j) continue;
+
+            if (str.charAt(i) === str.charAt(j)) {
+                flag = true;
+                break;
+            }
+        }
+
+        if (!flag) {
+            return str.charAt(i);
+        }
+    }
+
+    return "";
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
